9dbeb2c10f07332ad1aaad982142443becf9aa80
devjanaprime <dev@primeacademy.io>
2017-09-18 11:31:18 -0500

    updated js

--- scripts/ngLaunchpad.js
+++ scripts/ngLaunchpad.js
@@ -6,13 +6,13 @@ myApp.controller( 'LaunchpadController', function(){
   // hold our controller in a variable
   var vm = this;
   console.log( 'NG' );
-  // // add a two-way bound array to hold movies
-  // this.favoriteMovies = [];
-  // // two-way bound function expression to get more movies
-  // this.getMovieName = function(){
-  //   console.log( 'in getMovieName():', this.movieName );
-  //   // push new info into this array
-  //   this.favoriteMovies.push( this.movieName );
-  //   console.log( 'favoriteMovies:', this.favoriteMovies );
-  // }; // end getMovieName
+  // add a two-way bound array to hold movies
+  this.favoriteMovies = [];
+  // two-way bound function expression to get more movies
+  this.getMovieName = function(){
+    console.log( 'in getMovieName():', this.movieName );
+    // push new info into this array
+    this.favoriteMovies.push( this.movieName );
+    console.log( 'favoriteMovies:', this.favoriteMovies );
+  }; // end getMovieName
 }); // end LaunchpadControler
